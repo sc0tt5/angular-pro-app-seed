2e95a276c5bc06f0224c9d831dcd4ed7a2483649
Scott Robertson <sc0tt5@users.noreply.github.com>
2019-03-09 23:24:32 -0600

    Using ngTemplateOutlet with context

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,11 +4,16 @@ import { Component } from '@angular/core';
     selector: 'app-root',
     template: `
         <div>
-            <ng-container [ngTemplateOutlet]="tmpl"> </ng-container>
-            <ng-template #tmpl>
-                Todd Motto : England, UK
+            <ng-container [ngTemplateOutlet]="tmpl" [ngTemplateOutletContext]="ctx"></ng-container>
+            <ng-template #tmpl let-name let-location="location">
+                {{ name }} : {{ location }}
             </ng-template>
         </div>
     `
 })
-export class AppComponent {}
+export class AppComponent {
+    ctx = {
+        $implicit: 'Todd Motto',
+        location: 'England, UK'
+    };
+}
