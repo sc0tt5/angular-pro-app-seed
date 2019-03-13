11943c24eaddc4af4f81b0af26a4b156632b967b
Scott Robertson <sc0tt5@users.noreply.github.com>
2019-03-12 21:46:05 -0500

    Creating a custom attribute Directive

--- src/app/credit-card/credit-card.directive.ts
+++ src/app/credit-card/credit-card.directive.ts
@@ -1,9 +1,22 @@
 import { Directive, ElementRef } from '@angular/core';
 
+/**
+ * a component is basically a directive, but it has a template
+ * directives do not have templates, but can use the same life-cycle
+ * hooks as components
+ */
+
 @Directive({
-    selector: 'credit-card'
+    selector: '[credit-card]'
 })
+/**
+ * ^ same as component, we have a selector, but directives are typically attributes
+ * like <div credit-card></div>, so we will use a JS selector (square brackets) to
+ * wrap [credit-card]
+ *
+ */
 export class CreditCardDirective {
+    // inject element as type ElementRef into constructor
     constructor(private element: ElementRef) {
         console.log(this.element);
     }
