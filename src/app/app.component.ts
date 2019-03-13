c2390b257f80f5f5cc80136861b1299347a2e5fb
Scott Robertson <sc0tt5@users.noreply.github.com>
2019-03-12 21:29:00 -0500

    setup for next

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,58 +1,19 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
-
-// component decoration called "changeDetection"
-// ChangeDetectionStrategy.Default -- default runs everytime we change something
+import { Component } from '@angular/core';
 
 @Component({
     selector: 'app-root',
-    changeDetection: ChangeDetectionStrategy.Default,
     template: `
         <div>
-            <button (click)="addProp()">Add property</button>
-            <button (click)="changeUser()">Change user object</button>
-            <button (click)="changeName()">Change name property</button>
-            <div class="users">
-                <example-one [user]="user"></example-one>
-                <example-two [user]="user"></example-two>
-            </div>
+            <label>
+                Credit Card Number
+                <input
+                    name="credit-card"
+                    type="text"
+                    placeholder="Enter your 16-digit card number"
+                    credit-card
+                />
+            </label>
         </div>
     `
 })
-
-// component one uses Default
-// component two uses OnPush
-// ...otherwise these are identical
-// the below methods with change prop, name, and the entire user to see how these differ
-export class AppComponent {
-    user: any = {
-        name: 'Mark Hoppus',
-        age: 44,
-        location: 'California'
-    };
-
-    // clicking "Add property" adds to Default but not OnPush
-    addProp() {
-        this.user.email = 'blink@blink-182.net';
-    }
-
-    // clicking "Change name" only updates Default
-    changeName() {
-        this.user.name = 'Travis Barker';
-    }
-
-    changeUser() {
-        this.user = {
-            name: 'Tom Delonge',
-            age: 41,
-            location: 'California'
-        };
-    }
-
-    // so if we use immutable data object angular does not have to check previous
-    // which means angular will check much faster and doesn't have to check every property
-
-    // the ChangeDetectionStrategy Default has to check every property which means it will be slower
-
-    // the main idea is to use immutable data scructures along with ChangeDetectionStrategy OnPush
-    // to make change detection and application code much faster
-}
+export class AppComponent {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
-import { ExampleOneComponent } from './one/one.component';
-import { ExampleTwoComponent } from './two/two.component';
+import { CreditCardDirective } from './credit-card/credit-card.directive';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    ExampleOneComponent,
-    ExampleTwoComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  bootstrap: [
-    AppComponent
-  ]
+    declarations: [AppComponent, CreditCardDirective],
+    imports: [BrowserModule],
+    bootstrap: [AppComponent]
 })
 export class AppModule {}
