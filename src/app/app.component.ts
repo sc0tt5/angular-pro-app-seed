7500bddb93638e1dd308dfd488a6d24d72ad1669
Scott Robertson <sc0tt5@users.noreply.github.com>
2019-03-12 22:32:58 -0500

    setup for next

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,28 +1,39 @@
 import { Component } from '@angular/core';
 
 @Component({
-    selector: 'app-root',
-    template: `
-        <div>
-            <label>
-                Credit Card Number
-                <input
-                    name="credit-card"
-                    type="text"
-                    placeholder="Enter your 16-digit card number"
-                    credit-card
-                />
-            </label>
-            <!-- tooltip directive exportAs tooltip ...see here -->
-            <!-- anything we want to control is now bound to myTooltip -->
-            <label tooltip="3 digits, back of your card" #myTooltip="tooltip">
-                Enter your security code
-                <span (mouseover)="myTooltip.show()" (mouseout)="myTooltip.hide()">
-                    (?)
-                </span>
-                <input type="text" />
-            </label>
-        </div>
-    `
+  selector: 'app-root',
+  template: `
+    <div>
+      <ul>
+        <li *myFor="let item of items; let i = index;">
+          {{ i }} Member: {{ item.name | json }}
+        </li>
+        <template myFor [myForOf]="items" let-item let-i="index">
+          <li>
+            {{ i }} Member: {{ item.name | json }}
+          </li>
+        </template>
+      </ul>
+    </div>
+  `
 })
-export class AppComponent {}
+export class AppComponent {
+  items = [{
+    name: 'Mark Hoppus',
+    age: 44,
+    location: 'California'
+  },{
+    name: 'Tom Delonge',
+    age: 41,
+    location: 'California'
+  },{
+    name: 'Travis Barker',
+    age: 41,
+    location: 'California'
+  }];
+  constructor() {
+    setTimeout(() => {
+      this.items = [...this.items, { name: 'Matt Skiba', age: 40, location: 'California' }];
+    }, 2000);
+  }
+}
