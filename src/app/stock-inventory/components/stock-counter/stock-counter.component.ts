acff6db04aa5f801d555b311fb4e77986a637100
Scott Robertson <sc0tt5@users.noreply.github.com>
2019-03-23 12:58:52 -0500

    Implementing a ControlValueAccessor

--- src/app/stock-inventory/components/stock-counter/stock-counter.component.ts
+++ src/app/stock-inventory/components/stock-counter/stock-counter.component.ts
@@ -1,7 +1,18 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, forwardRef } from '@angular/core';
+import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
+
+// we will use the ControlValueAccessor to extend our component to talk to form groups
+const COUNTER_CONTROL_ACCESSOR = {
+    provide: NG_VALUE_ACCESSOR, // gains access to the existing value accessor
+    // forwardRef makes sure the component is available
+    // tslint:disable-next-line: no-use-before-declare
+    useExisting: forwardRef(() => StockCounterComponent), // which component to use to control user access
+    multi: true
+};
 
 @Component({
     selector: 'stock-counter',
+    providers: [COUNTER_CONTROL_ACCESSOR],
     styleUrls: ['./stock-counter.component.scss'],
     template: `
         <div class="stock-counter">
@@ -21,22 +32,46 @@ import { Component, Input } from '@angular/core';
         </div>
     `
 })
-export class StockCounterComponent {
+export class StockCounterComponent implements ControlValueAccessor {
     @Input() step: number = 10;
     @Input() min: number = 10;
     @Input() max: number = 1000;
 
     value: number = 10;
 
+    // private methods for registerOnTouch and registerOnChange
+    // we wrap them and store them internally
+    // tslint:disable-next-line: ban-types
+    private onTouch: Function;
+    // tslint:disable-next-line: ban-types
+    private onModelChange: Function;
+
+    // like when implementing ngOnInit, we have to impement the ControlValueAccessor interface <mat-chip-list class="mat-chip-list-stacked">
+
+    registerOnTouched(fn) {
+        this.onTouch = fn;
+    }
+    registerOnChange(fn) {
+        this.onModelChange = fn;
+    }
+
+    writeValue(value) {
+        this.value = value || 0;
+    }
+
     increment() {
         if (this.value < this.max) {
             this.value = this.value + this.step;
+            this.onModelChange(this.value); // this will update the json value
         }
+        this.onTouch(); // notify the control has changed
     }
 
     decrement() {
         if (this.value > this.min) {
             this.value = this.value - this.step;
+            this.onModelChange(this.value); // this will update the json value
         }
+        this.onTouch(); // notify the control has changed
     }
 }
--- src/app/stock-inventory/components/stock-selector/stock-selector.component.ts
+++ src/app/stock-inventory/components/stock-selector/stock-selector.component.ts
@@ -15,8 +15,12 @@ import { Product } from './../../models/product.interface';
                         {{ product.name }}
                     </option>
                 </select>
-                <input type="number" step="10" min="10" max="1000" formControlName="quantity" />
-                <stock-counter [step]="10" [min]="10" [max]="1000"></stock-counter>
+                <stock-counter
+                    [step]="10"
+                    [min]="10"
+                    [max]="1000"
+                    formControlName="quantity"
+                ></stock-counter>
                 <button type="button" (click)="onAdd()">Add stock</button>
             </div>
         </div>
