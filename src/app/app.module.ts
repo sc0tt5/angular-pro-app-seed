5b8233dbbc8e61dfcef652c9d8eafd0be8c018af
Scott Robertson <sc0tt5@users.noreply.github.com>
2019-03-30 22:39:43 -0500

    Custom preloading strategies

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,27 +1,39 @@
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { RouterModule, Routes, PreloadAllModules } from '@angular/router';
+import { RouterModule, Route, Routes, PreloadingStrategy } from '@angular/router';
+import { Observable, of } from 'rxjs';
 
 import { AppComponent } from './app.component';
 import { MailModule } from './mail/mail.module';
+
+export class CustomPreload implements PreloadingStrategy {
+    preload(route: Route, fn: () => Observable<any>): Observable<any> {
+        return route.data && route.data.preload ? fn() : of(null);
+    }
+}
 
 export const ROUTES: Routes = [
     // we create a new path, but we reference the module so we can lazy load
     // we need to #DashboardModule in case there are multiple modules
     // and in the DashboardModule, we remove the path 'dashboard'
-    { path: 'dashboard', loadChildren: './dashboard/dashboard.module#DashboardModule' },
-    { path: '**', redirectTo: 'folder/inbox' }
+    {
+        path: 'dashboard',
+        data: { preload: true }, // use our CustomPreload
+        loadChildren: './dashboard/dashboard.module#DashboardModule'
+    },
+    { path: '**', redirectTo: 'mail/folder/inbox' }
 ];
 
 @NgModule({
     declarations: [AppComponent],
+    providers: [CustomPreload],
     imports: [
         BrowserModule,
         HttpClientModule,
         MailModule,
         /* removed DashboardModule so it doesn't load at runtime ...we will now lazy laod */
-        RouterModule.forRoot(ROUTES, { preloadingStrategy: PreloadAllModules })
+        RouterModule.forRoot(ROUTES, { preloadingStrategy: CustomPreload }) // preload everything
     ],
     bootstrap: [AppComponent]
 })
