70219234179d501e1e2c12796f71d03761d83387
Scott Robertson <sc0tt5@users.noreply.github.com>
2019-04-02 21:47:10 -0500

    Guards with canActivateChild

--- src/app/auth/auth.guard.ts
+++ src/app/auth/auth.guard.ts
@@ -1,10 +1,10 @@
 import { Injectable } from '@angular/core';
-import { CanLoad, CanActivate } from '@angular/router';
+import { CanLoad, CanActivateChild } from '@angular/router';
 
 import { AuthService } from './auth.service';
 
 @Injectable()
-export class AuthGuard implements CanLoad, CanActivate {
+export class AuthGuard implements CanLoad, CanActivateChild {
     constructor(private authService: AuthService) {}
 
     canLoad() {
@@ -13,5 +13,8 @@ export class AuthGuard implements CanLoad, CanActivate {
 
     canActivate() {
         return this.authService.isLoggedIn();
+    }
+    canActivateChild() {
+        return false;
     }
 }
