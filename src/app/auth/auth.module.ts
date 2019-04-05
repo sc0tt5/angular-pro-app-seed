9427900e3998e4a63b369fe8cd467234e85ec53d
Scott Robertson <sc0tt5@users.noreply.github.com>
2019-04-04 21:11:52 -0500

    unit testing project setup

--- src/app/auth/auth.module.ts
+++
@@ -1,8 +0,0 @@
-import { NgModule } from '@angular/core';
-import { AuthGuard } from './auth.guard';
-import { AuthService } from './auth.service';
-
-@NgModule({
-    providers: [AuthService, AuthGuard]
-})
-export class AuthModule {}
