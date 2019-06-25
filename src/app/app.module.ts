a2859b47cde93827734a6ae7363a56efc5f2d3f5
Scott Robertson <sc0tt5@users.noreply.github.com>
2019-06-24 19:12:51 -0500

    Zones and NgZone

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,10 @@
-import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
-import { FoodStoreModule } from './food-store/food-store.module';
 
 @NgModule({
     declarations: [AppComponent],
-    imports: [
-        BrowserModule,
-        HttpClientModule,
-        FoodStoreModule.forRoot({ storeId: 10292, storeToken: 'eca938c99a0e9ff91029dc' })
-    ],
-    bootstrap: [AppComponent],
-    providers: []
+    imports: [BrowserModule],
+    bootstrap: [AppComponent]
 })
 export class AppModule {}
