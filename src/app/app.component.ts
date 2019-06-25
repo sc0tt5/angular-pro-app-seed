4e1439a11b0bbbc13545ac896caeb2de84fe4483
Scott Robertson <sc0tt5@users.noreply.github.com>
2019-06-24 19:05:14 -0500

    Configurable NgModules

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,24 +1,13 @@
 import { Component } from '@angular/core';
+import { FoodStoreService } from './food-store/food-store.service';
 
 @Component({
     selector: 'app-root',
-    styles: [
-        `
-            pizza-viewer,
-            side-viewer,
-            drink-viewer {
-                display: block;
-                border-bottom: 2px solid #eee;
-                padding: 20px 0;
-            }
-        `
-    ],
     template: `
-        <div>
-            <pizza-viewer></pizza-viewer>
-            <side-viewer></side-viewer>
-            <drink-viewer></drink-viewer>
-        </div>
+        <div>Food Store ({{ (store | async)?.name }})</div>
     `
 })
-export class AppComponent {}
+export class AppComponent {
+    store = this.foodService.getStore();
+    constructor(private foodService: FoodStoreService) {}
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,20 +2,16 @@ import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
-import { DrinkViewerComponent } from './containers/drink-viewer.component';
-import { PizzaViewerComponent } from './containers/pizza-viewer.component';
-import { SideViewerComponent } from './containers/side-viewer.component';
-import { API_TOKEN } from './token';
+import { FoodStoreModule } from './food-store/food-store.module';
 
 @NgModule({
-    declarations: [
-        AppComponent,
-        PizzaViewerComponent,
-        DrinkViewerComponent,
-        SideViewerComponent
+    declarations: [AppComponent],
+    imports: [
+        BrowserModule,
+        HttpClientModule,
+        FoodStoreModule.forRoot({ storeId: 10292, storeToken: 'eca938c99a0e9ff91029dc' })
     ],
-    imports: [BrowserModule, HttpClientModule],
     bootstrap: [AppComponent],
-    providers: [{ provide: API_TOKEN, useValue: '/api/pizzas' }]
+    providers: []
 })
 export class AppModule {}
