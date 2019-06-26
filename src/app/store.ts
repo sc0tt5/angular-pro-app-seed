ccccc08865a88e32c24694c9f31553ed8c381c13
Scott Robertson <sc0tt5@users.noreply.github.com>
2019-06-25 19:16:53 -0500

    Creating an Observable Store with Rx

--- src/app/store.ts
+++ src/app/store.ts
@@ -8,18 +8,25 @@ const state: State = {
     playlist: undefined
 };
 
+/*
+  store.set('todos', [{},{}])
+  store.select('todos')
+*/
 export class Store {
     private subject = new BehaviorSubject<State>(state);
     private store = this.subject.pipe(distinctUntilChanged());
 
+    // store.value
     get value() {
         return this.subject.value;
     }
 
+    // store.select<Todo[]>('todos')
     select<T>(name: string): Observable<T> {
         return this.store.pipe(pluck(name));
     }
 
+    // store.set('todos', [{},{}])
     // tslint:disable-next-line: no-shadowed-variable
     set(name: string, state: any) {
         this.subject.next({
